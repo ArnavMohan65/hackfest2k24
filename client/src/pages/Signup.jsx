1f0671c3acd0c3755073a739ebe73a08802aaae3
Arnav Mohan <114442113+ArnavMohan65@users.noreply.github.com>
2024-03-10 02:36:05 +0530

    Update Signup.jsx

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import axios from "axios";
-import "./Signup.css";
+import "./signup.css";
 
 const Signup = () => {
   const [formData, setFormData] = useState({
